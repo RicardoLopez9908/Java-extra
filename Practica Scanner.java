8153dddab7208c49e1a107093bd77e2b31ba3de0
RicardoLopez9908 <57273461+RicardoLopez9908@users.noreply.github.com>
2020-12-27 13:32:14 -0300

    Create Practica Scanner.java
    clase 1

---
+++ Practica Scanner.java
@@ -0,0 +1,32 @@
+//importar el scanner para usarlo
+import java.util.Scanner;
+public class hola_mundo {
+
+	public static void main(String[] args) {
+		//declaramos las variables
+		/*recordemos que podemos usar tambien:
+		 * enteros: byte/short/int/long
+		 * con coma: floar/double*/
+		String nombre;
+		int edad;
+		boolean valor;
+				
+		
+		// pedimos el nombre
+		System.out.println("hola ¿cual es tu nombre?");
+		Scanner input = new Scanner (System.in);
+		nombre= input.next();
+		
+		//pedimos la edad
+		System.out.println(nombre + "¿cuantos años tenes?");
+		Scanner input2 = new Scanner (System.in);
+		edad= input2.nextInt();
+		
+		//pedimos el valor boolean
+		System.out.println(edad + "¿true o false?");
+		Scanner input3 = new Scanner (System.in);
+		valor= input3.nextBoolean();
+		
+		//imprimo todas las variables
+		System.out.println(nombre +" de "+ edad +" "+ valor );
+	}
